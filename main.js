332a4bfb1b3623e3a12ae39e3015c22dcc4753c3
sangwooyu <dbtkddn598@gmail.com>
2022-07-04 15:16:17 +0900

    쿠키를 활용한 로그인 기능 추가

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
+var cookie = require('cookie');
 
 const { syncBuiltinESMExports } = require('module');
+const { ifError } = require('assert');
 
 //js로 만든 html파일은 결국 서버의 입장에서 파일경로를 새로 구성해서 넣어줘야함.
 //분리된 파일로 관리할경우 js에 html 파일에 <style>태그로 집어넣어주면 js로 만든 html 파일에 css 적용이 가능함.
-function templateHTML(title,list,description, control){
+function templateHTML(title,list,description, control, authStatusUI){
  return `<!doctype html> 
  <html>
  <head>
@@ -16,6 +18,7 @@ function templateHTML(title,list,description, control){
    <link rel="stylesheet" href="./style.css">
  </head>
  <body>
+   ${authStatusUI}
    <h1><a href="/">WEB</a></h1>
    <h2>${list}</h2>  
        ${control}
@@ -26,6 +29,13 @@ function templateHTML(title,list,description, control){
  `;
 }
 
+function authStatusUI(request,response){
+  var authStatusUI ='<a href="/login">login</a>';
+  if(authIsOwner(request,response)){
+    authStatusUI='<a href="/logout_process">logout</a>';
+  }
+  return authStatusUI;
+}
 function templateList(filelist){
   var list = '<ul>';
   var i = 0;
@@ -37,14 +47,24 @@ function templateList(filelist){
   return list;
 }
 
+function authIsOwner(request,response){
+  var isOwner = false;
+  var cookies={};
+  if(request.headers.cookie){
+    cookies = cookie.parse(request.headers.cookie);
+   }
+  if(cookies.email =='egoing777@gmail.com' && cookies.password === '111111'){
+    isOwner = true;
+  }
+  console.log(cookies);
+  return isOwner;
+}
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var title = queryData.id;
-    var pathname = url.parse(_url,true).pathname;
-    // console.log(queryData.id);
-    // console.log(pathname);
+    var pathname = url.parse(_url,true).pathname;  
 
 var list ='<ul>';
 var control =  ``;
@@ -72,7 +92,7 @@ if(pathname ==='/'){
         
     fs.readFile(`data/${queryData.id}`,'utf-8', function(err,description)
       {
-        var template = templateHTML(title, list, description ,control);
+        var template = templateHTML(title, list, description, control, authStatusUI(request,response));
        
         response.writeHead(200);
         response.end(template);
@@ -102,30 +122,56 @@ if(pathname ==='/'){
       </form>
       `;
 
-    var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title} </title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <h2>${list}</h2>  
-        <a href="/create">create</a>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
+    var template = templateHTML(title, list, description, control, authStatusUI(request,response));
       response.writeHead(200);
       response.end(template);
     }, 100);
 
       //내가 list파일에 <H2>태그 안 씌우니.. 위에서 계속 문제인 것마냥 오류를 계속 호출했음...
       // 
-   
-   
   }
+  else if(pathname ==='/login'){
+    fs.readdir('./data',function(err,filelist){   // 이 구문을 벗어나면 filelist 값들을 전달받은 전역변수 dirlist의 메모리에서 다 사라짐.. 
+      var title = 'Login';
+      var description =  
+      `<form action="login_process" method="post">
+          <p><input type="text" name="email" placeholder="email"></p>
+          <p><input type="password" name="password" placeholder="password"></p>
+          <p><input type="submit"></p>
+        <form>`;
+
+      var list = templateList(filelist);
+      var html = templateHTML(title,list,description,'');
+      response.writeHead(200);
+      response.end(html);
+    });
+    
+  }
+  else if(pathname === '/login_process'){
+    var body='';
+    request.on('data',function(data){
+      body += data;
+    });
+    request.on('end',function(){
+      var post = qs.parse(body);
+      if(post.email === 'egoing777@gmail.com' && post.password === '111111'){
+      response.writeHead(302,{
+        'Set-Cookie':[
+          `email=${post.email}`,
+          `password=${post.password}`,
+          `nickname=egoing`
+        ],
+        Location:`/` 
+       });
+    
+      response.end();
+      }
+      else{
+        response.end('Who?');
+      }
+    });
+
+  }    // console.log(list); // 분명히 list는 전역으로 밖에 선언했지만 이 read 함수를 벗어나는 순간 갖고 있는 값들이 사라짐..      
   else if(pathname === '/create_process'){
     var body='';
     request.on('data',function(data){
@@ -161,7 +207,9 @@ if(pathname ==='/'){
         <input type="submit">
         </p>
         </form>
-        `,control);
+        `, 
+        control,
+        authStatusUI(request,response));
         response.writeHead(200);
         response.end(template);
       })
